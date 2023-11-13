38983d498832c6a59ee4049bc0ef801858f2d3d4
David Abellán Navarro <projects@iberocode.com>
2023-11-13 19:14:55 +0100

    [IC-0001] feat: Creación de proyecto Laravel, con reducción de peso.

---
+++ app/Http/Controllers/UpdateStock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UpdateStock extends Controller {
+    public function index() {
+        return 'Hola mundo';
+    }
+}
